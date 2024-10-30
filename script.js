0f930c1a0f9b80bc55c64d2b7d85709e0c1220fe
LoughreaIndianCommunity <loughreaindiancc@gmail.com>
2024-10-30 11:43:54 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // JavaScript for toggling the sidenav
 function toggleSidenav() {
     const sidenav = document.querySelector('.sidenav');
-    if (sidenav.style.width === '0px' || sidenav.style.width === '') {
-        sidenav.style.width = '250px'; // Open the sidenav
-    } else {
-        sidenav.style.width = '0'; // Close the sidenav
-    }
+    sidenav.style.width = sidenav.style.width === '250px' ? '0' : '250px';
 }
